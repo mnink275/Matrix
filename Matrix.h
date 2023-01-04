9502d9953d123e7a881188d4d25f7d1d8b20c4eb
Niyaz <nmingaripov@gmail.com>
2023-01-04 21:19:21 +0300

    operator* overloaded for int*int, int*double, double*double and others

--- Matrix.h
+++ Matrix.h
@@ -9,7 +9,14 @@ class Matrix
 {
 public:
 	Matrix();
+	Matrix(T);
 	void print();
+
+	std::array<T, Column>& operator[](size_t idx);
+
+	template<class T, class U, int M, int N, int K>
+	friend Matrix<T, M, K> operator*(Matrix<U, M, N>&, Matrix<T, N, K>&);
+
 private:
 	std::array<std::array<T, Column>, Row> matrix;
 };
@@ -29,6 +36,19 @@ Matrix<T, Row, Column>::Matrix()
 
 
 template<class T, int Row, int Column>
+Matrix<T, Row, Column>::Matrix(T value)
+{
+	for (size_t i = 0; i < Row; i++)
+	{
+		for (size_t j = 0; j < Column; j++)
+		{
+			matrix[i][j] = value;
+		}
+	}
+}
+
+
+template<class T, int Row, int Column>
 void Matrix<T, Row, Column>::print()
 {
 	for (size_t i = 0; i < Row; i++)
@@ -39,4 +59,32 @@ void Matrix<T, Row, Column>::print()
 		}
 		std::cout << "\n";
 	}
+}
+
+
+template<class T, int Row, int Column>
+std::array<T, Column>& Matrix<T, Row, Column>::operator[](size_t idx)
+{
+	return matrix[idx];
+}
+
+
+template<class T, class U, int M, int N, int K>
+Matrix<T, M, K> operator*(Matrix<U, M, N>& mtx1, Matrix<T, N, K>& mtx2)
+{
+	Matrix<T, M, K> product;
+	// each element in product of mtx1 and mtx2
+	for (size_t i = 0; i < M; i++)
+	{
+		for (size_t j = 0; j < K; j++)
+		{
+			// Row-Column rule
+			for (size_t k = 0; k < N; k++)
+			{
+				product[i][j] += mtx1[i][k] * mtx2[k][j];
+			}
+		}
+	}
+
+	return product;
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 
 int main()
 {
-    Matrix<int, 5, 15> mtx;
+    Matrix<int, 2, 3> mtx1(1);
+    Matrix<int, 3, 2> mtx2(2.2);
+
+    Matrix<int, 2, 2> mtx = mtx1 * mtx2;
     mtx.print();
 
     return 0;
