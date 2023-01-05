cc9886291a15ee676d99daec71e7af29bd273487
Niyaz <nmingaripov@gmail.com>
2023-01-05 12:00:02 +0300

    added 2 copy constructor, det() funct (only for double)

--- Matrix.h
+++ Matrix.h
@@ -10,18 +10,24 @@ class Matrix
 public:
 	Matrix();
 	Matrix(T);
-	
+	~Matrix() = default;
+
+	Matrix(Matrix<T, R, C>& mtx);
+	Matrix<T, R, C>& operator=(Matrix<T, R, C>& mtx);
 	
 	void print();
-
+	/*T findMinor(size_t row, size_t column);*/
+	T det();
 	std::array<T, C>& operator[](size_t idx);
-	Matrix<T, R, C>& operator+(Matrix<T, R, C>&);
+	Matrix<T, R, C>& operator+(Matrix<T, R, C>& mtx);
 
 	template<class T, class U, int M, int N, int K>
-	friend Matrix<T, M, K>& operator*(Matrix<U, M, N>&, Matrix<T, N, K>&);
+	friend Matrix<T, M, K>& operator*(Matrix<U, M, N>& mtx1, Matrix<T, N, K>& mtx2);
 	
 
 private:
+	/*T detHelper(size_t row, size_t column);*/
+
 	std::array<std::array<T, C>, R> matrix;
 };
 
@@ -53,6 +59,21 @@ Matrix<T, R, C>::Matrix(T value)
 
 
 template<class T, int R, int C>
+Matrix<T, R, C>::Matrix(Matrix<T, R, C>& mtx)
+{
+	matrix = mtx.matrix;
+}
+
+
+template<class T, int R, int C>
+Matrix<T, R, C>& Matrix<T, R, C>::operator=(Matrix<T, R, C>& mtx)
+{
+	matrix = mtx.matrix;
+	return *this;
+}
+
+
+template<class T, int R, int C>
 void Matrix<T, R, C>::print()
 {
 	for (size_t i = 0; i < R; i++)
@@ -64,6 +85,53 @@ void Matrix<T, R, C>::print()
 		std::cout << "\n";
 	}
 }
+
+
+//template<class T, int R, int C>
+//T Matrix<T, R, C>::findMinor(size_t row, size_t column)
+//{
+//	/*return pow(-1, i + j) * matrix[i][j] * minorHelper(i, j);*/
+//}
+
+
+template<class T, int R, int C>
+T Matrix<T, R, C>::det()
+{
+	auto gauss = matrix;
+	for (size_t k = 0; k < R - 1; k++)
+	{
+		for (size_t i = k + 1; i < R; i++)
+		{
+			double delta = -gauss[i][k] / gauss[k][k];
+			for (size_t j = k; j < R; j++)
+			{
+				gauss[i][j] += gauss[k][j] * delta;
+			}
+		}
+	}
+	
+	double ans = 1;
+	for (size_t k = 0; k < C; k++)
+	{
+		ans *= gauss[k][k];
+	}
+
+	return ans;
+}
+
+
+//template<class T, int R, int C>
+//T Matrix<T, R, C>::detHelper(size_t row, size_t skip_idx)
+//{
+//	T ans = 0;
+//	for (size_t j = 0; j < C; j++)
+//	{
+//		if (j == skip_idx) continue;
+//		ans += pow(-1, row + j) * matrix[row][j] * detHelper(row + 1, j);
+//	}
+//
+//	return ans;
+//}
 
 
 template<class T, int R, int C>
--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 
 int main()
 {
-    Matrix<int, 3, 3> mtx1(1);
-    Matrix<int, 3, 3> mtx2(2);
-
-    Matrix<int, 3, 3> mtx = mtx1 * mtx2;
+    Matrix<double, 2, 2> mtx;
+    mtx[0][0] = 1;
+    mtx[0][1] = 2;
+    mtx[1][0] = 3;
+    mtx[1][1] = 4;
     mtx.print();
+    /*Matrix<double, 3, 3> mtx = mtx1 * mtx2;
+    mtx.print();*/
+    std::cout << mtx.det();
 
     return 0;
 }
